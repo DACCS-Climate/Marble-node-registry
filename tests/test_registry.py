ca34ca360d45598572d404e83a1a5683aeddd864
mishaschwartz <4380924+mishaschwartz@users.noreply.github.com>
2023-07-11 09:14:27 -0400

    update comments and var names in tests

--- tests/test_registry.py
+++ tests/test_registry.py
@@ -18,7 +18,7 @@ def links_json_schema(requests_mock, request):
 
 @pytest.fixture
 def registry_content(request):
-    """Return the content contained in ../doc/node_registry.example.json"""
+    """Return the content contained in node_registry.json"""
     root_dir = os.path.dirname(os.path.dirname(request.fspath))
     example_registry = os.path.join(root_dir, "node_registry.json")
     with open(example_registry) as f:
@@ -28,10 +28,10 @@ def registry_content(request):
 
 @pytest.fixture
 def schema_content(request):
-    """Return the content contained in ../doc/node_registry.example.json"""
+    """Return the content contained in node_registry.schema.json"""
     root_dir = os.path.dirname(os.path.dirname(request.fspath))
-    example_registry = os.path.join(root_dir, "node_registry.schema.json")
-    with open(example_registry) as f:
+    example_schema = os.path.join(root_dir, "node_registry.schema.json")
+    with open(example_schema) as f:
         content = json.load(f)
     return content
 
